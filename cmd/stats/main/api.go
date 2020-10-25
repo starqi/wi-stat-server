c7dd9f76db60ee88e7261f8204369bb08be66980
starqi <helloimzirui@gmail.com>
2020-10-24 20:11:38 -0400

    Add CORS, tested with main project

--- cmd/stats/main/api.go
+++ cmd/stats/main/api.go
@@ -28,6 +28,18 @@ func main() {
     hdb = hsql.MakeHiscoresDb(db)
 
     router := gin.Default()
+    router.Use(func (c *gin.Context) {
+        c.Header("Access-Control-Allow-Origin", "http://localhost:8081")
+        c.Header("Access-Control-Allow-Methods", "*")
+        c.Header("Access-Control-Allow-Credentials", "true")
+        c.Header("Access-Control-Allow-Headers", "Authorization, *")
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(http.StatusNoContent)
+        } else {
+            c.Next()
+        }
+    })
+
     basicAuthGroup := router.Group("/hiscore", gin.BasicAuth(gin.Accounts {
         "me": "123",
     }))
@@ -41,18 +53,18 @@ func getTopHiscores(c *gin.Context) {
     c.JSON(http.StatusOK, dbHiscoresToJson(hiscores))
 }
 
-// curl -d '[{"name":"hank","team":"sutasu","kills":0,"deaths":400}]' -H 'Content-Type: application/json' -i localhost:8080/hiscore
+// curl -d '[{"name":"hank","team":"sutasu","kills":0,"deaths":400}]' -H 'Content-Type:application/json' -u me:123 -i localhost:8080/hiscore
 func postHiscore(c *gin.Context) {
     var json []HiscoreEntry
     if err := c.ShouldBindJSON(&json); err != nil {
-        c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err})
+        c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
         return
     }
     hdb.Insert(jsonHiscoresToDb(json))
     c.Status(http.StatusOK)
 }
 
-// curl localhost:8080/hiscore/top
+// curl -i -u me:123 localhost:8080/hiscore/top
 func dbHiscoresToJson(hiscores []hsql.HiscoreEntry) []HiscoreEntry {
     a := make([]HiscoreEntry, 0, len(hiscores))
     for _, h := range hiscores {
