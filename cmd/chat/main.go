6813fb99a02a6b7e6150c35ee8c588ce318f0d4f
starqi <helloimzirui@gmail.com>
2021-02-10 14:24:05 -0500

    Working basic implementation

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -9,9 +9,15 @@ import (
     "strings"
 )
 
+type client struct {
+    conn *websocket.Conn
+    revision uint64
+}
+
 type messages struct {
     arr []string
     index int
+    revision uint64
 }
 
 func makeMessages(size int) messages {
@@ -25,61 +31,79 @@ func makeMessages(size int) messages {
 func (r *messages) add(s string) {
     r.arr[r.index] = s
     r.index = (r.index + 1) % len(r.arr)
+    r.revision++
 }
 
-func (r *messages) forEach(cb func(int, string)) {
+func full_modulo(index int, length int) int {
+    // -3 -2 -1 0 1 2 3 4 5 -> 0 -2 -1 0 1 2 0 1 2 -> 312012012 -> 012012012
+    return ((index % length) + length) % length
+}
+
+func (r *messages) forEach(amount int, cb func(int, string)) {
     l := len(r.arr)
-    for i := 0; i < l; i++ {
-        index := r.index - 1 - i
-        if index < 0 {
-            index = l + (index % l);
-        }
-        item := r.arr[index]
+    if amount < 0 || amount > l { amount = l; }
+
+    // Start from latest message minus amount
+    start := full_modulo(r.index - amount, l)
+    for _i := start; _i < start + amount; _i++ {
+        i := full_modulo(_i, l)
+
+        item := r.arr[i]
         if item == "" {
             break;
         }
-        cb(index, item)
+        cb(i, item)
     }
 }
 
+var register chan *websocket.Conn
+var unregister chan *client
 var inbound chan string
-var broadcast chan string
-var clients map[*websocket.Conn]bool
+var clients map[*client]bool
 
 func main() {
 
-    inbound = make(chan string)
-    broadcast = make(chan string)
-    clients = make(map[*websocket.Conn]bool)
+    register = make(chan *websocket.Conn)
+    unregister = make(chan *client)
+    inbound = make(chan string, 20)
+    clients = make(map[*client]bool)
 
     go aggregator()
-    go broadcaster()
 
     router := gin.Default()
     router.GET("/chat", chat)
     router.Run()
 }
 
+// TODO Force tick if revision delta >= length on inbound, to avoid message loss on mass inbound
 func aggregator() {
-    outboundTicker := time.NewTicker(2000 * time.Millisecond)
+    outboundTicker := time.NewTicker(500 * time.Millisecond)
     msgs := makeMessages(20)
     for {
         select {
         case m := <-inbound:
             msgs.add(m)
         case <-outboundTicker.C:
-            msgs.forEach(func (i int, s string) {
-                broadcast <- s
-            })
-        }
-    }
-}
-
-func broadcaster() {
-    for {
-        s := <-broadcast
-        for c, _ := range clients {
-            c.WriteMessage(websocket.TextMessage, []byte(s))
+            for c := range clients {
+                num_to_send := msgs.revision - c.revision;
+                if num_to_send > 0 {
+                    log.Printf("%s - rev=%d, curr=%d", c.conn.RemoteAddr().String(), msgs.revision, c.revision)
+                    msgs.forEach(int(num_to_send), func (i int, s string) {
+                        //log.Print("Message ", i)
+                        c.conn.WriteMessage(websocket.TextMessage, []byte(s))
+                    })
+                    c.revision = msgs.revision
+                }
+            }
+        case conn := <-register:
+            c := client {
+                conn: conn,
+                revision: 0, // Get all messages on init
+            }
+            clients[&c] = true
+            go client_loop(&c)
+        case c := <-unregister:
+            clients[c] = false
         }
     }
 }
@@ -103,20 +127,16 @@ func chat(c *gin.Context) {
         log.Print("Chat init failed! ", err)
         return
     }
-    go client(conn)
+    register <- conn
     c.Status(http.StatusOK);
 }
 
-func client(conn *websocket.Conn) {
-    clients[conn] = true
-    defer func() {
-        clients[conn] = false
-    }()
-
+func client_loop(c *client) {
     for {
-        messageType, p, err := conn.ReadMessage()
+        messageType, p, err := c.conn.ReadMessage()
         if err != nil {
             log.Print("Read message failed! ", err)
+            unregister <- c
             return
         }
         if messageType == websocket.TextMessage {
